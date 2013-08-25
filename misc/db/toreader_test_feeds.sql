e42c2364ec58909eca26b688e94fbbbe679e8630
unknown <webmast44@mail.ru>
2013-08-25 12:58:37 +0300

    Change default db to not have any values

--- misc/db/toreader_test_feeds.sql
+++ misc/db/toreader_test_feeds.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys = ON;
 
-
-INSERT INTO feed VALUES(NULL, 0, 'Хабрахабр', 'http://habrahabr.ru/rss/best/', 'http://habrahabr.ru/', 0, 3600, NULL);
-INSERT INTO feed VALUES(NULL, 0, 'Корреспондент', 'http://k.img.com.ua/rss/ru/news.xml', 'http://korrespondent.net/', 0, 3600, NULL);
+-- INSERT INTO feed VALUES(NULL, 0, 'Хабрахабр', 'http://habrahabr.ru/rss/best/', 'http://habrahabr.ru/', 0, 3600, NULL);
+-- INSERT INTO feed VALUES(NULL, 0, 'Корреспондент', 'http://k.img.com.ua/rss/ru/news.xml', 'http://korrespondent.net/', 0, 3600, NULL);
 INSERT INTO feed VALUES(NULL, 0, 'Perl blogs', 'http://blogs.perl.org/atom.xml', 'http://blogs.perl.org/', 0, 3600, NULL);
+INSERT INTO feed VALUES(NULL, 0, 'BBC News', 'http://feeds.bbci.co.uk/news/rss.xml', 'http://www.bbc.com/news/', 0, 3600, NULL);
