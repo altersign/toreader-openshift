a01eab4e4b1be7e9b3fe0ab6361ffcc118068727
unknown <webmast44@mail.ru>
2013-08-26 03:03:40 +0300

    Remove BBC, add CNN

--- misc/db/toreader_test_feeds.sql
+++ misc/db/toreader_test_feeds.sql
@@ -2,6 +2,6 @@ PRAGMA foreign_keys = ON;
 
 --                      id    0  name          RSS url                           site url                  0  update in seconds  NULL
 INSERT INTO feed VALUES(NULL, 0, 'Perl blogs', 'http://blogs.perl.org/atom.xml', 'http://blogs.perl.org/', 0, 3600,              NULL);
-INSERT INTO feed VALUES(NULL, 0, 'BBC News', 'http://feeds.bbci.co.uk/news/rss.xml', 'http://www.bbc.com/news/', 0, 3600, NULL);
+INSERT INTO feed VALUES(NULL, 0, 'CNN News', 'http://rss.cnn.com/rss/edition.rss', 'http://edition.cnn.com/', 0, 600, NULL);
 -- INSERT INTO feed VALUES(NULL, 0, 'Хабрахабр', 'http://habrahabr.ru/rss/best/', 'http://habrahabr.ru/', 0, 3600, NULL);
 -- INSERT INTO feed VALUES(NULL, 0, 'Корреспондент', 'http://k.img.com.ua/rss/ru/news.xml', 'http://korrespondent.net/', 0, 3600, NULL);
